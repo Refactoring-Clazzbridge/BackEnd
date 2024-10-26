2f71e6fe02fedde9dce0bd28eaf9d97372fdd2e9
Bulgogi-Pizza <quitendexit@gmail.com>
2024-10-26 16:52:16 +0900

    [#40] fix : 컨트롤러 URL 중복 문제 수정

--- src/main/java/com/example/academy/controller/VoteController.java
+++ src/main/java/com/example/academy/controller/VoteController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,7 +68,7 @@ public class VoteController {
     }
   }
 
-  @PostMapping
+  @PutMapping
   @Operation(summary = "투표 선택", security = {@SecurityRequirement(name = "bearerAuth")})
   public ResponseEntity<?> doVote(@RequestBody DoVoteDTO doVoteDTO) {
     try {
