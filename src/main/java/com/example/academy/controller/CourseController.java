0e2ada33f3715f290fd0700db9dbc06d4861c810
tkddn0321 <godsknight58@naver.com>
2024-10-29 15:49:49 +0900

    fetch : course 날짜 조회

--- src/main/java/com/example/academy/controller/CourseController.java
+++ src/main/java/com/example/academy/controller/CourseController.java
@@ -1,21 +1,18 @@
 package com.example.academy.controller;
 
+
 import com.example.academy.dto.course.CourseAddDTO;
 import com.example.academy.dto.course.CourseTitleDTO;
 import com.example.academy.dto.course.CourseUpdateDTO;
 import com.example.academy.dto.course.GetCourseDTO;
-import com.example.academy.dto.member.MemberDTO;
-import com.example.academy.dto.member.StudentDTO;
-import com.example.academy.dto.course.SelectCourseDTO;
 import com.example.academy.service.CourseService;
-import com.example.academy.service.StudentCourseService;
-import com.example.academy.service.SubmissionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,39 +28,29 @@ public class CourseController {
 
     @Autowired
     private final CourseService courseService;
-    private final StudentCourseService studentCourseService;
 
-    public CourseController(CourseService courseService,
-        StudentCourseService studentCourseService) {
+    public CourseController(CourseService courseService) {
         this.courseService = courseService;
-        this.studentCourseService = studentCourseService;
     }
+
+
 
     @Operation(summary = "강의명 전체 조회", security = {@SecurityRequirement(name = "bearerAuth")})
     @GetMapping("/title")
-    public ResponseEntity<List<?>> getCourseTitle() {
+    public ResponseEntity<List<?>> getCourseTitle(){
         List<CourseTitleDTO> title = courseService.getCourseTitle();
         return ResponseEntity.ok(title);
     }
 
-    @Operation(summary = "강의에 속한 수강생 리스트 조회", security = {
-        @SecurityRequirement(name = "bearerAuth")})
-    @GetMapping("/{courseId}/students")
-    public ResponseEntity<List<StudentDTO>> getStudentsByCourseId(
-        @PathVariable(value = "courseId") Long courseId) {
-        List<StudentDTO> students = studentCourseService.getStudentsByCourseId(courseId);
-        return ResponseEntity.ok(students);
-    }
 
     @Operation(summary = "강의 전체 조회", security = {@SecurityRequirement(name = "bearerAuth")})
     @GetMapping
-    public ResponseEntity<List<GetCourseDTO>> getAllCourse() {
+    public ResponseEntity<List<GetCourseDTO>> getAllCourse(){
         return ResponseEntity.ok(courseService.getAllCourse());
     }
-
     @Operation(summary = "강의 조회", security = {@SecurityRequirement(name = "bearerAuth")})
     @GetMapping("{id}")
-    public ResponseEntity<?> getCourse(@PathVariable Long id) {
+    public ResponseEntity<?> getCourse(@PathVariable Long id){
         try {
             List<GetCourseDTO> getCourseDTOS = courseService.getCourse(id);
             return ResponseEntity.ok(getCourseDTOS);
@@ -71,6 +58,13 @@ public class CourseController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
+
+    @Operation(summary = "강의 시간 조회", security = {@SecurityRequirement(name = "bearerAuth")})
+    @GetMapping("time/{title}")
+    public ResponseEntity<?> getTime(@PathVariable String title){
+        return ResponseEntity.ok(courseService.getTime(title));
+    }
+
 
 
     @PostMapping
@@ -84,7 +78,7 @@ public class CourseController {
         }
     }
 
-    @PutMapping
+    @PutMapping()
     @Operation(summary = "강의 변경", security = {@SecurityRequirement(name = "bearerAuth")})
     public ResponseEntity<?> updateCourse(@RequestBody CourseUpdateDTO courseUpdateDTO) {
         try {
@@ -95,32 +89,18 @@ public class CourseController {
         }
     }
 
+
+
     @DeleteMapping("{id}")
     @Operation(summary = "강의 삭제", security = {@SecurityRequirement(name = "bearerAuth")})
-    public ResponseEntity<?> deleteCourse(@PathVariable Long id) {
+    public ResponseEntity<?> deleteCourse(@PathVariable Long id){
         try {
             courseService.deleteCourse(id);
             return ResponseEntity.ok("삭제완료");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error 강의 삭제 실패");
         }
-    } // 여기 괄호 추가됨
 
-    @Operation(summary = "강의실 강의 선택")
-    @GetMapping("/select")
-    public ResponseEntity<List<SelectCourseDTO>> SeatAllCourse() {
-        return ResponseEntity.ok(courseService.seatAllCourse());
     }
 
-    @Operation(summary = "강의 번호 반환")
-    @GetMapping("/teacher")
-    public ResponseEntity<Long> getTeacherByCourseId() {
-        return ResponseEntity.ok(courseService.getTeacherByCourseId());
-    }
-
-    @Operation(summary = "학생의 강의 번호 반환")
-    @GetMapping("/student")
-    public ResponseEntity<Long> getStudentCourseId() {
-        return ResponseEntity.ok(courseService.getStudentCourseId());
-    }
 }
