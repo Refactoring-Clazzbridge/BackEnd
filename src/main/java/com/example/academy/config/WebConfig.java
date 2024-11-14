21af5b3e68d0d2df4c5209c66d96d964ed5c02bf
Seulki Kang <skbb11@naver.com>
2024-11-14 12:00:48 +0900

    fix:url2차 수정

--- src/main/java/com/example/academy/config/WebConfig.java
+++ src/main/java/com/example/academy/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://175.106.99.175:3000", "http://localhost:3000") // 프론트엔드 URL
+        .allowedOrigins("http://175.106.99.175", "http://localhost:3000") // 프론트엔드 URL
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*") // Authorization 헤더 허용
         .allowCredentials(true);
