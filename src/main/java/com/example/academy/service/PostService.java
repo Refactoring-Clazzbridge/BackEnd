72bb4ca806693446da0ab5e2e0f91d76e1c089b5
ftfg777 <ftfgz5093@gmail.com>
2024-11-14 10:17:31 +0900

    feat: getCourseAllPost 로직 변경

--- src/main/java/com/example/academy/service/PostService.java
+++ src/main/java/com/example/academy/service/PostService.java
@@ -290,9 +290,15 @@ public class PostService {
         if (!posts.isEmpty()) {
             return Stream.concat(generalAnnouncements.stream(), posts.stream())
                 .map(postResponseMapper::toDto)
-                .sorted(Comparator.comparing((PostResponseDTO post) ->
-                        post.getCourseId() == null ? 0 : 1)  // courseId가 null인 게시글을 상위로 배치
-                    .thenComparing(PostResponseDTO::getId, Comparator.reverseOrder()))
+                .sorted(Comparator
+                    .comparing((PostResponseDTO post) -> post.getCourseId() == null ? 0
+                        : 1) // courseId가 null인 게시글을 상위로 배치
+                    .thenComparing(
+                        (PostResponseDTO post) -> BoardTypes.공지사항.name().equals(post.getBoardType())
+                            ? 0
+                            : 1) // boardType이 '공지사항'인 게시글을 그 다음에 배치
+                    .thenComparing(PostResponseDTO::getId,
+                        Comparator.reverseOrder())) // ID 내림차순으로 정렬
                 .collect(Collectors.toList());
         } else {
             return Collections.emptyList(); // 빈 리스트 반환
