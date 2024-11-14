5ad501e687ed733f94f4d2112ab38288b199fc7b
Seulki Kang <skbb11@naver.com>
2024-11-14 12:15:53 +0900

    f

--- src/main/java/com/example/academy/config/WebConfig.java
+++ src/main/java/com/example/academy/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://175.106.99.175", "http://localhost:3000") // 프론트엔드 URL
+        .allowedOrigins("http://175.106.99.175", "http://localhost:3000","http://k.clazzbridge.o-r.kr") // 프론트엔드 URL
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*") // Authorization 헤더 허용
         .allowCredentials(true);
